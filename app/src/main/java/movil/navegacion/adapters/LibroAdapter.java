93cb190ea6097bfacdd4bd8dee9fa222c5fdb1e2
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-04-04 12:34:42 -0500

    Template, Modelo Libro Implementado

---
+++ app/src/main/java/movil/navegacion/adapters/LibroAdapter.java
@@ -0,0 +1,41 @@
+package movil.navegacion.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import movil.navegacion.models.Libro;
+
+/**
+ * Created by Dario Chamorro on 4/04/2016.
+ */
+public class LibroAdapter extends BaseAdapter {
+
+    Context context;
+    List<Libro> data;
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/movil/navegacion/models/Libro.java
@@ -0,0 +1,25 @@
+package movil.navegacion.models;
+
+/**
+ * Created by Dario Chamorro on 4/04/2016.
+ */
+public class Libro {
+
+    String nombre, autor;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
