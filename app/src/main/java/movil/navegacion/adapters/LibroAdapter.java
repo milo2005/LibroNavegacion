54fe7395266e0fbfd00b5d9b9f739df5e1e88d07
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-04-04 12:49:23 -0500

    LibroAdapter implemented

--- app/src/main/java/movil/navegacion/adapters/LibroAdapter.java
+++ app/src/main/java/movil/navegacion/adapters/LibroAdapter.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
+import movil.navegacion.R;
 import movil.navegacion.models.Libro;
 
 /**
@@ -17,25 +19,40 @@ public class LibroAdapter extends BaseAdapter {
     Context context;
     List<Libro> data;
 
-
+    public LibroAdapter(Context context, List<Libro> data) {
+        this.context = context;
+        this.data = data;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return data.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return data.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View v = convertView;
+        if(v == null)
+            v = View.inflate(context, R.layout.template_libro,null);
+
+        Libro l = data.get(position);
+
+        TextView nombre = (TextView) v.findViewById(R.id.nombre);
+        TextView autor = (TextView) v.findViewById(R.id.autor);
+
+        nombre.setText(l.getNombre());
+        autor.setText(l.getAutor());
+
+        return v;
     }
 }
